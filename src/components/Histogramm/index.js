ad93c184c6a644e97c61ad754dc7a44f679cba3d
Edward <mordviskiy@mail.ru>
2018-01-28 23:28:41 +0200

    Spinner as app loader

--- src/components/Histogramm/index.js
+++ src/components/Histogramm/index.js
@@ -1,8 +1,20 @@
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import * as d3 from 'd3'
 import ReactFauxDOM from 'react-faux-dom'
 
 export default class Histogram extends PureComponent {
+  static propTypes = {
+    data: PropTypes.arrayOf(PropTypes.shape({
+      days: PropTypes.arrayOf(PropTypes.number).isRequired,
+      week: PropTypes.number.isRequired
+    }))
+  }
+
+  static defaultProps = {
+    data: []
+  }
+
   renderD3 = () => {
     const { data } = this.props
     const _data = data.reduce((arr, week) => {
@@ -26,7 +38,8 @@ export default class Histogram extends PureComponent {
     const svg = d3.select(ReactFauxDOM.createElement('svg'))
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
-    const g = svg.append('g')
+
+    svg.append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`)
 
     const xScale = d3.scaleTime()
@@ -54,7 +67,7 @@ export default class Histogram extends PureComponent {
     const sum = (d) => d.reduce((sum, el) => {
       return sum + el.val
     }, 0)
-    const formatCount = d3.format(',.0f')
+    // const formatCount = d3.format(',.0f')
 
     const bar = svg.selectAll('.bar')
       .data(bins)
@@ -62,7 +75,7 @@ export default class Histogram extends PureComponent {
       .attr('class', 'bar')
       .attr('transform', (d) => `translate(${xScale(d.x0)},${yScale(sum(d))})`)
 
-    const rects = bar.append('rect')
+    bar.append('rect')
       .attr('x', 1)
       .attr('fill', 'steelblue')
       .attr('width', (d) => xScale(d.x1) - xScale(d.x0) - 1)
--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,3 +1,27 @@
+import React from 'react'
+
+import './styles.scss'
+
 export default function () {
-  return '...'
+  return (
+    <div styleName='loader-container'>
+      <svg
+        styleName='spinner'
+        width='65px'
+        height='65px'
+        viewBox='0 0 66 66'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <circle
+          styleName='circle'
+          fill='none'
+          strokeWidth='6'
+          strokeLinecap='round'
+          cx='33'
+          cy='33'
+          r='30'
+        />
+      </svg>
+    </div>
+  )
 }
